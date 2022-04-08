c2b8a7b975dd6fa94539890f79597a53844c8596
Brad Sorour <brad.sorour@gmail.com>
2022-04-08 11:49:30 +0100

    simple session info

--- mydiary/app/controllers/application_controller.rb
+++ mydiary/app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+    before_action :count_requests_in_session
+    def count_requests_in_session
+        session[:requests] ||=0
+        session[:requests] += 1
+    end
 end
